527ad0e7deed1e3fba3c51d1b30c9c3119a97984
Toufeeq Pasha <toufeeq@sovereignwallet.network>
2022-06-07 16:16:11 +0530

    Added demo connection config

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ const METABLOCKCHAIN_PROVIDER = {
   LOCAL: 'ws://127.0.0.1:9944',
   DEV: 'wss://n3testnet.metabit.exchange',
   TESTNET: 'wss://n3testnet.metabit.exchange',
+  DEMO: 'wss://demo.metamui.money',
   MAINNET: ['wss://n2.metamui.id', 'wss://n1.metamui.id', 'wss://n3.metamui.id', 'wss://n4.metamui.id'],
 };
 
